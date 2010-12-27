4204a4cbe7b54650fe4cde13af698a4d8d6b1e8a
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-27 15:55:51 +0900

    initial commit

---
+++ lib/Plack/Session/State/MobileAgentID.pm
@@ -0,0 +1,33 @@
+package Plack::Session::State::MobileAgentID;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Session::State::MobileAgentID -
+
+=head1 SYNOPSIS
+
+  use Plack::Session::State::MobileAgentID;
+
+=head1 DESCRIPTION
+
+Plack::Session::State::MobileAgentID is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
